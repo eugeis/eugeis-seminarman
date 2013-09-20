34af76b628e06cb8d97dd9d4041f681cc8be0e52
Eugen Eisler <eoeisler@gmail.com>
2013-09-21 00:00:32 +0200

    Fix Teilnehmer calculation

--- admin/controllers/courses.php
+++ admin/controllers/courses.php
@@ -629,12 +629,6 @@ JModel::addIncludePath(JPATH_ADMINISTRATOR . DS . 'components' . DS . 'com_users
 				$templateData[$key] = $templateData[$key] + 1;
 			}
 			
-			//count attendees per status
-			foreach ($attendees as $k => $v) {
-				$key = 'ATTENDEES_STATUS_'.$v['STATUS_ID'];
-				$templateData[$key] = $templateData[$key] + 1;
-			}
-			
 			//remove all with status not 1
 			foreach ($attendees as $k => $v) {
 				if($v['STATUS_ID'] != 1) {
